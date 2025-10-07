8f645d4c065968b928cdc4889b3ed8d4a079d9a3
alanaamerico22 <145569370+alanaamerico22@users.noreply.github.com>
2025-10-07 12:18:36 -0300

    fff

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
 const perguntas = [
     {
         enunciado: "Como você controla os gastos mensais?",
         alternativas: [
             {
-                texto: "Aplicativos de controle financeiro (planilhas digitais, apps de orçamento)."
-                afirmacao: " Você acredita que registrar tudo em tempo real ajuda a manter as contas sob controle e evitar surpresas no fim do mês.  ",
+                texto: "Aplicativos de controle financeiro (planilhas digitais, apps de orçamento).",
+                afirmacao: "Você acredita que registrar tudo em tempo real ajuda a manter as contas sob controle e evitar surpresas no fim do mês.",
             },
             {
-                texto: "Anotações à mão em um caderno ou agenda física",
-                afirmacao: "“Você prefere o método tradicional porque sente que escrever reforça a consciência dos gastos.",
+                texto: "Anotações à mão em um caderno ou agenda física.",
+                afirmacao: "Você prefere o método tradicional porque sente que escrever reforça a consciência dos gastos.",
             }
         ]
     },
@@ -23,20 +22,20 @@ const perguntas = [
         enunciado: "Quando recebe um dinheiro extra (salário bônus, devolução), qual é sua primeira atitude?",
         alternativas: [
             {
-                texto: "Guardar parte para uma reserva de emergência ou objetivo futuro.",
-                afirmacao: “Você vê esse recurso como uma oportunidade de fortalecer sua segurança financeira.”,
+                texto: " Guardar parte para uma reserva de emergência ou objetivo futuro.",
+                afirmacao: "Você vê esse recurso como uma oportunidade de fortalecer sua segurança financeira.",
             },
-    {
-        texto: " Usar imediatamente para pagar dívidas ou realizar algum desejo imediato.”",
-        afirmacao: "Você prefere aplicar o valor onde ele trará alívio imediato ou satisfação pessoal.",
-    }
-]
+            {
+                texto: "Usar imediatamente para pagar dívidas ou realizar algum desejo imediato.",
+                afirmacao: "Você prefere aplicar o valor onde ele trará alívio imediato ou satisfação pessoal.",
+            }
+        ]
     },
-{
-    enunciado: "Como você costuma decidir quanto vai poupar a cada mês?",
+    {
+        enunciado: "Como você costuma decidir quanto vai poupar a cada mês?",
         alternativas: [
             {
-                texto: "Definir um percentual fixo da renda (ex.: 15 % do salário)",
+                texto: "Definir um percentual fixo da renda (ex.: 15 % do salário).",
                 afirmacao: "Você acredita que reservar uma porcentagem constante facilita o controle e cria o hábito de economizar.",
             },
             {
@@ -44,9 +43,9 @@ const perguntas = [
                 afirmacao: "Você prefere adaptar a poupança à realidade de cada período, ajustando‑se às variações de gasto.",
             }
         ]
-},
-{
-    enunciado: "Quando recebe um crédito em cartão de loja, qual a sua postura?",
+    },
+    {
+        enunciado: "Quando recebe um crédito em cartão de loja, qual a sua postura?",
         alternativas: [
             {
                 texto: "Pagar a fatura integralmente no vencimento para evitar juros.",
@@ -57,29 +56,31 @@ const perguntas = [
                 afirmacao: "Você valoriza a flexibilidade de fluxo de caixa, aceitando pagar juros futuros para resolver prioridades atuais.",
             }
         ]
-},
-{
-    enunciado: "Qual estratégia você usa para controlar gastos supérfluos?",
+    },
+    {
+        enunciado: "Qual estratégia você usa para controlar gastos supérfluos?",
         alternativas: [
             {
                 texto: "Criar “limites de despesa” por categoria (lazer, delivery, roupas) em um aplicativo de orçamento.",
                 afirmacao: "Você sente que limites pré‑definidos evitam surpresas e mantêm o orçamento equilibrado.",
             },
             {
-                texto: "Esperar 24 horas antes de fazer uma compra não planejada e reavaliar a real necessidade",
+                texto: "Esperar 24 horas antes de fazer uma compra não planejada e reavaliar a real necessidade.",
                 afirmacao: "Você acredita que o intervalo de tempo reduz compras impulsivas e ajuda a refletir sobre prioridades.",
             }
         ]
-},
+    },
 ];
 
+let atual = 0; 
+let perguntaAtual; 
 
-let atual = 0;
-let perguntaAtual;
-
-function mostraPergunta() {
-    perguntaAtual = pergunta[atual];
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+
 }
 
 mostraPergunta();
+
+
